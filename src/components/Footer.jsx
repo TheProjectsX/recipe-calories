95a19f304064f9a756e2f1c51d89f2617a4d8065
TheProjectsX <manvstech2.0@gmail.com>
2024-03-16 01:14:23 -0700

    Footer Created

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <div className="p-8 text-center bg-black text-white">
+      <h1 className="text-2xl font-bold">Recipe Calories</h1>
+      <p className="mb-6">Get your desired Recipe from Our Hub!</p>
+
+      <small>Copyright &copy; 2024. All Rights Reserved</small>
+    </div>
+  );
+};
+
+export default Footer;
